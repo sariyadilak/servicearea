278e1e41fe10571734e1a78f5dd0aa8df4e45756
sariyadilak <ariyadilak.s@gmail.com>
2020-03-14 13:43:45 +0700

    add marker and delete draggable marker

--- js/app.js
+++ js/app.js
@@ -30,31 +30,41 @@ window.addEventListener('resize', () => map.getViewPort().resize());
 export { router, geocoder }
 
 
-/* ...
- * map and platform code from previous section goes here
- * ...
- */
+//add marker
+// Define a variable holding SVG mark-up that defines an animated icon image:
+var animatedSvg =
+    '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" ' + 
+    'y="0px" style="margin:-112px 0 0 -32px" width="136px"' + 
+    'height="150px" viewBox="0 0 136 150"><ellipse fill="#000" ' +
+    'cx="32" cy="128" rx="36" ry="4"><animate attributeName="cx" ' + 
+    'from="32" to="32" begin="0s" dur="1.5s" values="96;32;96" ' + 
+    'keySplines=".6 .1 .8 .1; .1 .8 .1 1" keyTimes="0;0.4;1"' + 
+    'calcMode="spline" repeatCount="indefinite"/>' +    
+    '<animate attributeName="rx" from="36" to="36" begin="0s"' +
+    'dur="1.5s" values="36;10;36" keySplines=".6 .0 .8 .0; .0 .8 .0 1"' + 
+    'keyTimes="0;0.4;1" calcMode="spline" repeatCount="indefinite"/>' +
+    '<animate attributeName="opacity" from=".2" to=".2"  begin="0s" ' +
+    ' dur="1.5s" values=".1;.7;.1" keySplines=" .6.0 .8 .0; .0 .8 .0 1" ' +
+    'keyTimes=" 0;0.4;1" calcMode="spline" ' +
+    'repeatCount="indefinite"/></ellipse><ellipse fill="#1b468d" ' +
+    'cx="26" cy="20" rx="16" ry="12"><animate attributeName="cy" ' +
+    'from="20" to="20" begin="0s" dur="1.5s" values="20;112;20" ' +
+    'keySplines=".6 .1 .8 .1; .1 .8 .1 1" keyTimes=" 0;0.4;1" ' +
+    'calcMode="spline" repeatCount="indefinite"/> ' +
+    '<animate attributeName="ry" from="16" to="16" begin="0s" ' + 
+    'dur="1.5s" values="16;12;16" keySplines=".6 .0 .8 .0; .0 .8 .0 1" ' +
+    'keyTimes="0;0.4;1" calcMode="spline" ' +
+    'repeatCount="indefinite"/></ellipse></svg>';
 
-let polygon;
-const marker = new H.map.Marker(center, {volatility: true});
-marker.draggable = true;
+// Create an icon object, an object with geographic coordinates and a marker:
+var icon = new H.map.DomIcon(animatedSvg),
+    coords = {lat: -22.8906, lng: -43.2283},
+    marker = new H.map.DomMarker(coords, {icon: icon});
+
+// Set map center and zoom, add the marker to the map:
+map.setCenter(coords);
+map.setZoom(18);
 map.addObject(marker);
 
-// Add event listeners for marker movement
-map.addEventListener('dragstart', evt => {
-   if (evt.target instanceof H.map.Marker) behavior.disable();
-}, false);
-map.addEventListener('dragend', evt => {
-   if (evt.target instanceof H.map.Marker) {
-      behavior.enable();
-      calculateIsoline(); 
-   }
-}, false);
-map.addEventListener('drag', evt => {
-   const pointer = evt.currentPointer;
-   if (evt.target instanceof H.map.Marker) {
-     evt.target.setGeometry(map.screenToGeo(pointer.viewportX, pointer.viewportY));
-   }
-}, false);
 
 
